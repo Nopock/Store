62b32b048f132437491b4f89277649203beb7ef4
Nopock <78834703+Nopock@users.noreply.github.com>
2022-12-28 20:32:58 -0800

    Update DataTypeResources.kt

--- src/main/kotlin/org/hyrical/store/constants/DataTypeResources.kt
+++ src/main/kotlin/org/hyrical/store/constants/DataTypeResources.kt
@@ -16,19 +16,23 @@ object DataTypeResources  {
 
     val mongoCollections: MutableMap<String, MongoCollection<Document>> = mutableMapOf()
 
+    @JvmStatic
     fun enableRedisRepositories(uri: String) {
         redisClient = Jedis(uri)
     }
 
+    @JvmStatic
     fun enableRedisRepositories(host: String = "127.0.0.1", port: Int = 6379) {
         redisClient = Jedis(host, port)
     }
 
+    @JvmStatic
     fun enableMongoRepositories(uri: String, database: String) {
         mongoClient = MongoClients.create(uri)
         mongoDatabase = mongoClient!!.getDatabase(database)
     }
 
+    @JvmStatic
     fun enableMongoRepositories(host: String = "127.0.0.1", port: Int = 27017, database: String) {
         mongoClient = MongoClients.create("mongodb://$host:$port")
         mongoDatabase = mongoClient!!.getDatabase(database)
