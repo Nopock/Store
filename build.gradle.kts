b8d9ebdeb2187e9fd5ccbff8a0d0ff1f6f7c19a1
Nopock <78834703+Nopock@users.noreply.github.com>
2023-01-12 07:55:09 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,7 @@ dependencies {
     compileOnly("redis.clients:jedis:4.3.1")
     compileOnly("org.mongodb:mongo-java-driver:3.12.11")
     implementation("org.slf4j:slf4j-api:2.0.6")
-    //dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.20")
     testImplementation(kotlin("test"))
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     compileOnly("io.projectreactor:reactor-core:3.5.2")
 }
 
