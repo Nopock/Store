f38ba10d7b18636136b46c46b9514aae19f38aa9
Nopox <78834703+Nopock@users.noreply.github.com>
2023-01-23 10:36:47 -0800

    Corrected Import * [ Quick Fix ]

--- src/main/kotlin/org/hyrical/store/connection/mongo/details/impl/URIMongoDetails.kt
+++ src/main/kotlin/org/hyrical/store/connection/mongo/details/impl/URIMongoDetails.kt
@@ -1,5 +1,7 @@
 package org.hyrical.store.connection.mongo.details.impl
 
+import org.hyrical.store.connection.mongo.details.AbstractMongoDetail
+
 class URIMongoDetails(val uri: String) : AbstractMongoDetail() {
 
     override fun getURI(): String {
